df45080887df0eab991361f4eefee3e591b75593
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-19 20:17:18 +0200

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,7 @@
 module.exports = function (req, res, next) {
   if (req.session.user) {
-    // 1- check 
+    // 1- check cookie look for session id
+    // 2- check there exists a session by that session id
     next()
   } else {
     next({ status: 401, message: 'you cannot touch this' })
