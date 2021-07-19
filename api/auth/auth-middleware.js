69a24ba393fae5a64bdb9ea22e24a42855ed1978
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-19 20:14:11 +0200

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,1 +1,3 @@
-module.exports = 
+module.exports = function(req, res, next) {
+  
+}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
+const restricted = require('../auth/auth-middleware')
 const Users = require("./users-model.js");
 
 // we will protect this one
