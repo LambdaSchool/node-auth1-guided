0196b28a468b0e323a2e901955287e5838f39e26
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-19 20:14:27 +0200

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,7 +3,7 @@ const restricted = require('../auth/auth-middleware')
 const Users = require("./users-model.js");
 
 // we will protect this one
-router.get("/", (req, res, next) => {
+router.get("/", restricted, (req, res, next) => {
   Users.find()
     .then(users => {
       res.status(200).json(users);
