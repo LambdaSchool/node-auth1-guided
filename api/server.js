fb51fe33a90013cb8c9b4e6422af378a1a9b1a10
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-19 19:20:13 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ server.use('/api/auth', authRouter);
 server.get('/hello', (req, res) => {
   if (req.headers.cookie) {
     // the server has seen this client before
-    res.sendStatus(`<h1>hey, nice to see you again!</h1>`)
+    res.send(`<h1>hey, nice to see you again!</h1>`)
   } else {
     // it's the first visit by this client
     res.set('Set-Cookie', 'friend=yes; Max-Age=1000000;')
