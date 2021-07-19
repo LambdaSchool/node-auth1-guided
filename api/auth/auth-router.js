49ecb8fe1916d5848521f64fa4d4aee89990ed23
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-19 18:56:32 +0200

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -25,6 +25,7 @@ router.post('/login', (req, res, next) => {
   // 1- gather username and password from req.body
   // 2- check that the username is actually present in db
   // 3- use bcrypt to verify password against hash
+  // 4- start a session for that client
 })
 
 module.exports = router
