0c507a059e98863fef3e7c51f3b9674c2f77df63
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-19 18:31:16 +0200

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../users/users-model')
+const bcrypt = require('bcryptjs')
 
 router.post('/register', (req, res, next) => {
   // pull username and password from the request
   // validate username & password
-  // hash the password
+  // hash the password with bcrypt
 })
 
 router.post('/login', (req, res, next) => {
