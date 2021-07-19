eefabb5f224d4008b53d05aac989479150d01dfb
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-19 18:42:25 +0200

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -4,10 +4,10 @@ const User = require('../users/users-model')
 const bcrypt = require('bcryptjs')
 
 router.post('/register', async (req, res, next) => {
-  // pull username and password from the request
-  // validate username & password
-  // hash the password with bcrypt
-  // store user into db and send back nice message
+  // 1- pull username and password from the request
+  // (2- validate username & password)
+  // 3- hash the password with bcrypt
+  // 4- store user into db and send back nice message
   const { username, password } = req.body
   const hash = bcrypt.hashSync(password, 8) // 2 ^ 8
   const newUser = {
